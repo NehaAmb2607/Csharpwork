005f587089abdfc17cf9700bf451732a4bd0915a
ambraleneha26@outlook.com <ambraleneha26@outlook.com>
2022-05-17 10:33:51 +0530

    trying working

---
+++ ProjectBasics/ProjectBasics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProjectBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Variables and constants 
+            byte number =2;
+            int count = 56;
+            float totalprice = 1.26f;
+            char character = 'W';
+            string str = "Neha";
+            bool isOutput = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalprice);
+            Console.WriteLine(character);
+            Console.WriteLine(str);
+            Console.WriteLine(isOutput);
+            Console.WriteLine("{0} {1}", byte.MinValue ,byte.MaxValue);
+        }
+    }
+}
