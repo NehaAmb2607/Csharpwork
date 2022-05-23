0489a7bd6a8125619dfe7d548c61a0b07bafa1aa
ambraleneha26@outlook.com <ambraleneha26@outlook.com>
2022-05-23 16:32:44 +0530

    update basic work

--- ProjectBasics/ProjectBasics/Program.cs
+++ ProjectBasics/ProjectBasics/Program.cs
@@ -20,6 +20,58 @@ namespace ProjectBasics
             Console.WriteLine(str);
             Console.WriteLine(isOutput);
             Console.WriteLine("{0} {1}", byte.MinValue ,byte.MaxValue);
+
+            //Type conversion
+
+            //implicit type
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+
+            //explicit type 
+
+            long l = 10;
+            int c = (int)l;
+            Console.WriteLine(c);
+
+            //non-compatible type
+
+            string s = "1234";
+            long f= Convert.ToInt64(s);
+            Console.WriteLine(f);
+
+            //operators
+
+            var number1 = 10;
+            var number2 = 3;
+
+            //arithmetic operators
+            Console.WriteLine(number1 + number2 );
+            Console.WriteLine(number1 - number2);
+            Console.WriteLine(number1 / number2);
+            Console.WriteLine(number1 * number2);
+            Console.WriteLine(number1 % number2);
+
+            //comparison operators
+
+            Console.WriteLine(number1 == number2);
+            Console.WriteLine(number1 != number2);
+            Console.WriteLine(number1 > number2);
+            Console.WriteLine(number1 < number2);
+            Console.WriteLine(number1 >= number2);
+            Console.WriteLine(number1 <= number2);
+
+            //logical operators
+
+            var number3 = 1;
+
+            Console.WriteLine(number3 < number1 && number3 > number2);
+            Console.WriteLine(number1 != number3 || number1 != number2);
+
+
+
+
+
         }
     }
 }
